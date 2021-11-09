9ef8a5119187f24d4d5d0215694ce1d41e3748c1
mentari <erizkimentari@gmail.com>
2021-11-09 08:27:10 +0700

    NPM

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+// https://www.npmjs.com/
+const validator = require('validator');
+const chalk = require('chalk');
+
+// VALIDATOR
+// console.log(validator.isEmail('ai@mail.com'));
+// console.log(validator.isMobilePhone('838888888', 'id-ID'));
+// console.log(validator.isNumeric('838888888'));
+
+// CHALK
+// console.log(chalk.bgBlue.white.italic('Hello World'));
+const pesan = chalk`
+    There are {bold.red 5280 feet} in a mile.
+	In {bold.blue miles}, there are {black.bold.bgWhite six} feet.
+`;
+console.log(pesan);
