9ab16e5a66045a785c3d88927287c0cec682c534
unknown <erizkimentari@gmail.com>
2021-11-21 16:25:57 +0700

    TRY MONGODB

---
+++ 22-coba-mongo/app.js
@@ -0,0 +1,34 @@
+const { MongoClient} = require('mongodb')
+
+const uri = 'mongodb://127.0.0.1:27017'
+const dbName = 'wpu';
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+client.connect((error, client) => {
+    if (error) {
+        return console.log('Koneksi gagal!')
+    }
+    console.log('Koneksi Berhasil!')
+    
+    // pilih database
+    const db = client.db(dbName)
+    
+    // menambahkan 1 data ke collection mahasiswa
+    db.collection('mahasiswa').insertOne(
+        {
+            name: 'Lalisa',
+            email: 'lalisa@mail.com'
+        },
+        (error,result) => {
+            if (error) {
+                return console.log('Gagal menambahkan data!')
+            }
+            console.log(result)
+        }
+    )
+})
+
