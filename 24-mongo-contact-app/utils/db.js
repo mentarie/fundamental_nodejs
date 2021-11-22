b8da434c7a26955026cb31013cc35adfb25d014a
unknown <erizkimentari@gmail.com>
2021-11-23 06:17:16 +0700

    Mongo + NodeJS + Express

--- 24-mongo-contact-app/utils/db.js
+++ 24-mongo-contact-app/utils/db.js
@@ -2,19 +2,4 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/wpu', {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
-    // useCreateIndex: true
 });
-
-// // Menambah 1 data
-// const contact1 = new Contact({
-//     nama:  'Aiiii',
-//     nohp: '083462791891',
-//     email: 'ai@mail.com'
-// })
-
-// // Simpan ke collection
-// contact1
-//     .save()
-//     .then(contact => {
-//         console.log(contact)
-//     })
